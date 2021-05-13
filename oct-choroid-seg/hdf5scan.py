bc52fec60e95c5538b71cbdac860a667f0f61a2b
Paty Buendia <pbmail01@bellsouth.net>
2021-05-13 11:02:06 -0400

    Used Black to format

--- oct-choroid-seg/hdf5scan.py
+++ oct-choroid-seg/hdf5scan.py
@@ -1,68 +1,67 @@
 #!/usr/bin/python
 
-import sys, getopt
-import imageio
-import matplotlib.pyplot as plt
-import matplotlib.image
+import sys
+import getopt
 import numpy as np
 import h5py
 
 # Scan any hdf5 file
 def scan_hdf52(path, recursive=True, tab_step=2):
-	def scan_node(g, tabs=0):
-		elems = []
-		for k, v in g.items():
-			if isinstance(v, h5py.Dataset):
-				elems.append(v.name)
-			elif isinstance(v, h5py.Group) and recursive:
-				elems.append((v.name, scan_node(v, tabs=tabs + tab_step)))
-		return elems
-	with h5py.File(path, 'r') as f:
-		return scan_node(f)
+    def scan_node(g, tabs=0):
+        elems = []
+        for k, v in g.items():
+            if isinstance(v, h5py.Dataset):
+                elems.append(v.name)
+            elif isinstance(v, h5py.Group) and recursive:
+                elems.append((v.name, scan_node(v, tabs=tabs + tab_step)))
+        return elems
 
+    with h5py.File(path, "r") as f:
+        return scan_node(f)
 
 
 def main(argv):
-	infile = ''
-	outfile = ''
-	# Example: Run with 'python hdf5scan.py -i model_epoch858.hdf5 -o hdf5-scan.txt'
-	try:
-		opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    infile = ""
+    outfile = ""
+    # Example: Run with 'python hdf5scan.py -i model_epoch858.hdf5 -o hdf5-scan.txt'
+    try:
+        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
 
-		for opt, arg in opts:
-			if opt == '-h':
-				print('hdf5scan.py -i <inputfile> -o <outputfile>')
-				sys.exit()
-			elif opt in ("-i", "--ifile"):
-				infile = arg
-			elif opt in ("-o", "--ofile"):
-				outfile = arg
-	except getopt.GetoptError as e:
-		print('hdf5scan.py -i <inputfile> -o <outputfile>')
-		sys.exit(2)
-	if infile == '' or outfile == '':
-		print('hdf5scan.py -i <inputfile> -o <outputfile>')
-		sys.exit(2)
+        for opt, arg in opts:
+            if opt == "-h":
+                print("hdf5scan.py -i <inputfile> -o <outputfile>")
+                sys.exit()
+            elif opt in ("-i", "--ifile"):
+                infile = arg
+            elif opt in ("-o", "--ofile"):
+                outfile = arg
+    except getopt.GetoptError:
+        print("hdf5scan.py -i <inputfile> -o <outputfile>")
+        sys.exit(2)
+    if infile == "" or outfile == "":
+        print("hdf5scan.py -i <inputfile> -o <outputfile>")
+        sys.exit(2)
 
-	# write hdf5 contents to file
-	elems=scan_hdf52(infile,True,2)
+    # write hdf5 contents to file
+    elems = scan_hdf52(infile, True, 2)
 
-	ffile = open(outfile, "w+")
+    ffile = open(outfile, "w+")
 
-	for e in elems:
-		content = str(e)+str("\n")
-		ffile.write(content)
+    for e in elems:
+        content = str(e) + str("\n")
+        ffile.write(content)
 
-	ffile.close()
+    ffile.close()
 
-	# print summary to stdout
-	f=h5py.File(infile, 'r')
-	keys = list(f.keys())
+    # print summary to stdout
+    f = h5py.File(infile, "r")
+    keys = list(f.keys())
 
-	for k in keys:
-		n1 = f.get(k)
-		n1 = np.array(n1)
-		print("Keys: " + str(k) + ", Dims: " + str(n1.shape))
+    for k in keys:
+        n1 = f.get(k)
+        n1 = np.array(n1)
+        print("Keys: " + str(k) + ", Dims: " + str(n1.shape))
+
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
